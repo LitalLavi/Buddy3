5df14e5823c93a38d8a8ea2f64ae8febdf1254e3
LitalLavi <50523146+LitalLavi@users.noreply.github.com>
2021-09-10 18:35:14 +0300

    Add files via upload

---
+++ jScripts/all-page-content/click_food_data.js
@@ -0,0 +1,38 @@
+﻿export default [
+    {
+        id: "click_food_feeding",
+        headline: "האכלה",
+        allText: `אוכל מזון ארוחה ארוחות שעות גיל להאכיל`,
+    },
+
+    {
+        id: "click_food_howFeed",
+        headline: "איך להאכיל?",
+        allText: `אוכל מזון ארוחה ארוחות שעות גיל להאכיל קערה`,
+    },
+
+    {
+        id: "click_food_treats",
+        headline: "חטיפים",
+        allText: `חטיפים מוטיבציה השמנה חנויות טעים פינוק`,
+    },
+
+    {
+        id: "click_food_receivingFood",
+        headline: "נוהל קבלת מזון",
+        allText: `מזון מרכז אישור אוכל תיאבון ארוחות אספקה `,
+    },
+
+    {
+        id: "click_food_notEating",
+        headline: "הכלב לא אוכל מה לעשות?",
+        allText: `מזון אוכל חוסר תיאבון לא אוכל האכלה ארוחות`,
+    },
+
+    {
+        id: "click_food_drink",
+        headline: "שתייה",
+        allText: `שתייה מים לשתות לרוויה שותה`,
+    },
+
+];
---
+++ jScripts/all-page-content/click_home_data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+    id: "click_home_before",
+    headline: "הכנות בבית לקבלת הגור",
+    allText: `איך להתכונן מה לעשות לפני בית מסודר הכנה גור`,
+  },
+
+  {
+    id: "click_home_gettoknow",
+    headline: "היכרות עם הבית",
+    allText: `היכרות עם הבית גור סביבת מגורים מותר אסור השגחה `,
+  },
+
+  {
+    id: "click_home_toys",
+    headline: "צעצועים ולמה הם חשובים",
+    allText: `צעצועים לעיסה משחק כדורים לשחק גור חפצים אישיים חסר מעש גירויים`,
+  },
+
+  {
+    id: "click_home_cage",
+    headline: "כלוב",
+    allText: `כלוב שינה לישון צרכים מאורה פיפי בלילה מיטה `,
+    },
+
+    {
+        id: "click_home_cantOut",
+        headline: "אי אפשר לצאת מהבית",
+        allText: `תעסוקה מזג אוויר חם קר גשום בית יציאה טיול `,
+    },
+
+];
